f15b11b768595ec7cb5fbc1dfcbc0b42df3b5350
Nina Woods <woownina@users.noreply.github.com>
2022-08-19 14:58:09 +0000

    project

--- pythonproject/ascii.py
+++ pythonproject/ascii.py
@@ -1,12 +1,16 @@
 #!usr/bin/python3
 """ create program that prints user selected ASCII art"""
 
-#offer the user a menu of possible ASCII art to view
-menu =["Sailor Moon", "Sailor Mars", "Sailor Mercury", "Sailor Jupiter", "Sailor Venus"]
-print("Welcome to ASCII Art Selector! Please make your selection:", menu)
+def main():
+    """run time code"""
+    #offer the user a menu of possible ASCII art to view
+    menu =["SailorMoon", "Sailor Mars", "Sailor Mercury", "Sailor Jupiter", "Sailor Venus"]
+    print("Welcome to ASCII Art Selector! Please make your selection:", menu)
 
-#take user selection and display art
-selection = input()
+    #take user selection and display art
+    selection = input()
 
-f = open(selection.lower(), "r")
-print(f.read())
+    with open(selection.lower() + ".txt", "r", encoding="utf-8") as file:
+        print(file.read())
+
+main()
