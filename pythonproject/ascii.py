9f3b55557aca7262251eeb7c6124a13c20e06f15
Nina Woods <woownina@users.noreply.github.com>
2022-08-19 16:28:22 +0000

    final code

--- pythonproject/ascii.py
+++ pythonproject/ascii.py
@@ -1,16 +1,38 @@
 #!usr/bin/python3
+# pylint: disable=R1727
 """ create program that prints user selected ASCII art"""
 
 def main():
     """run time code"""
     #offer the user a menu of possible ASCII art to view
-    menu =["SailorMoon", "Sailor Mars", "Sailor Mercury", "Sailor Jupiter", "Sailor Venus"]
+    menu ={
+          "A": "Sailor Moon",
+          "B": "Sailor Mars",
+          "C": "Sailor Mercury",
+          "D": "Sailor Jupiter",
+          "E": "Sailor Venus"}
+
     print("Welcome to ASCII Art Selector! Please make your selection:", menu)
 
+
     #take user selection and display art
-    selection = input()
+    selection = input(menu.keys)
 
-    with open(selection.lower() + ".txt", "r", encoding="utf-8") as file:
-        print(file.read())
-
+    if selection == 'A' or 'a':
+        with open("sailormoon.txt","r", encoding="utf-8") as file:
+            print(file.read())
+    elif selection == 'B' or 'b':
+        with open("sailormars.txt","r", encoding="utf-8") as file:
+            print(file.read())
+    elif selection == 'C' or 'c':
+        with open("sailormercury.txt","r", encoding="utf-8") as file:
+            print(file.read())
+    elif selection == 'D' or 'd':
+        with open("sailorjupiter.txt","r", encoding="utf-8") as file:
+            print(file.read())
+    elif selection == 'E' or 'e':
+        with open("sailorvenus.txt","r", encoding="utf-8") as file:
+            print(file.read())
+    else:
+        print("Sorry try again")
 main()
